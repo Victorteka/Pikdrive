39b665dcf8927155dea498115c0f91465d2ddb3f
Victor Teka <ambukateka@gmail.com>
2021-04-21 19:09:36 +0300

    Add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-card class="card" elevation="2" outlined> </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+};
+</script>
+
+<style scoped>
+.card {
+  border-radius: 12px;
+}
+</style>
---
+++ src/components/Fab.vue
@@ -0,0 +1,12 @@
+<template>
+  <h4>Fab</h4>
+</template>
+
+<script>
+export default {
+  name: "Fab",
+};
+</script>
+
+<style>
+</style>
